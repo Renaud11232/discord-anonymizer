3ab7327a4fe2297c2e10e50891a83f0d4af9e7b3
Renaud Gaspard <gaspardrenaud@hotmail.com>
2022-04-03 10:14:58 +0200

    adds watching status

--- discordanonymizer/command_line.py
+++ discordanonymizer/command_line.py
@@ -9,6 +9,7 @@ def main():
     parser.add_argument("--token", type=str, default=os.environ.get("TOKEN"))
     parser.add_argument("--channel", type=int, default=os.environ.get("CHANNEL"))
     parser.add_argument("--log-level", type=str, default=os.environ.get("LOG_LEVEL") or "INFO")
+    parser.add_argument("--watching", type=str, default=os.environ.get("WATCHING") or "your DMs")
     args = parser.parse_args()
-    bot = DiscordAnonymizer(args.token, args.channel, args.log_level)
+    bot = DiscordAnonymizer(args.token, args.channel, args.log_level, args.watching)
     bot.run()
