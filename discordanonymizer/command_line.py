a6e4f7f56645d1b89956e94ed0ec16278826ddbd
Renaud Gaspard <gaspardrenaud@hotmail.com>
2022-04-03 10:22:15 +0200

    adds help text to the command line parser

--- discordanonymizer/command_line.py
+++ discordanonymizer/command_line.py
@@ -5,11 +5,27 @@ from discordanonymizer import DiscordAnonymizer
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Starts the discordanonymizer bot")
-    parser.add_argument("--token", type=str, default=os.environ.get("TOKEN"))
-    parser.add_argument("--channel", type=int, default=os.environ.get("CHANNEL"))
-    parser.add_argument("--log-level", type=str, default=os.environ.get("LOG_LEVEL") or "INFO")
-    parser.add_argument("--watching", type=str, default=os.environ.get("WATCHING") or "your DMs")
+    parser = argparse.ArgumentParser(description="Starts the DiscordAnonymizer bot")
+    parser.add_argument("--token",
+                        type=str,
+                        default=os.environ.get("TOKEN"),
+                        help="Bot token used to access Discord API. If omitted, the value of the `TOKEN` environment variable will be used"
+                        )
+    parser.add_argument("--channel",
+                        type=int,
+                        default=os.environ.get("CHANNEL"),
+                        help="Channel ID where message will be reposted. If omitted, the value of the `CHANNEL` environment variable will be used"
+                        )
+    parser.add_argument("--log-level",
+                        type=str,
+                        default=os.environ.get("LOG_LEVEL") or "INFO",
+                        help="Log level. If omitted, the value of the `LOG_LEVEL` environment variable will be used. Defaults to `INFO`"
+                        )
+    parser.add_argument("--watching",
+                        type=str,
+                        default=os.environ.get("WATCHING") or "your DMs",
+                        help="Watching status of the bot. If omitted, the value of the `WATCHING` environment variable will be used. Defaults to `your DMs`"
+                        )
     args = parser.parse_args()
     bot = DiscordAnonymizer(args.token, args.channel, args.log_level, args.watching)
     bot.run()
